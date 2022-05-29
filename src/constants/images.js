caaf498d4846bdaf57d873895c4bb556589fcec6
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-05-29 15:29:58 +0200

    Update the images.js file

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,5 @@
 import email from '../assets/email.png';
 import mobile from '../assets/mobile.png';
-import api from '../assets/api.png';
-import cpp from '../assets/cpp.png';
 import css from '../assets/css.png';
 import figma from '../assets/figma.png';
 import flutter from '../assets/flutter.png';
@@ -28,19 +26,9 @@ import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
 
-import adidas from '../assets/adidas.png';
-import amazon from '../assets/amazon.png';
-import asus from '../assets/asus.png';
-import bolt from '../assets/bolt.png';
-import nb from '../assets/nb.png';
-import skype from '../assets/skype.png';
-import spotify from '../assets/spotify.png';
-
 export default {
   email,
   mobile,
-  api,
-  cpp,
   css,
   figma,
   flutter,
@@ -64,11 +52,4 @@ export default {
   profile,
   circle,
   logo,
-  adidas,
-  amazon,
-  asus,
-  bolt,
-  nb,
-  skype,
-  spotify,
 };
