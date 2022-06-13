e4b4f6cf30fdeb43b3c0299242553a1775304e1b
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-06-13 18:28:32 +0200

    Change the project id and token

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: 'bp1tcvcf',
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: 'skYQ7rDbJtIgwtpRinC4oDISwZc3jlRnZ0E1RUTuKmP9ktFcdGoFamHph8VNDeDPXLLGzQCAKvXnPNHiXT2iu3uTXxRmXdXo23IYoKVcIV50WNNKOQSnfWxTRo69ulwErCPXEOedEKL8gcc1IXePSo5CLZ1QmG1Oh3RSMiTDjbhRLR5ovl83',
 });
 
 const builder = imageUrlBuilder(client);
