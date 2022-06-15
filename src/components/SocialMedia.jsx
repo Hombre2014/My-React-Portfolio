de061a106ae6d35c0e562510c771f80439d900c0
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-06-15 16:39:19 +0200

    Updates from portfolio review implemented

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -8,7 +8,7 @@ const SocialMedia = () => (
       <a href="https://twitter.com/Chamkoriyski" target="_blank" rel="noopener noreferrer"><BsTwitter /> </a>
     </div>
     <div>
-      <a href="https://twitter.com/Chamkoriyski" target="_blank" rel="noopener noreferrer"><BsLinkedin /> </a>
+      <a href="https://www.linkedin.com/in/axebit/" target="_blank" rel="noopener noreferrer"><BsLinkedin /> </a>
     </div>
     <div>
       <a href="https://github.com/Hombre2014" target="_blank" rel="noopener noreferrer"><BsGithub /> </a>
--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -72,6 +72,6 @@ const Testimonial = () => {
 
 export default AppWrap(
   MotionWrap(Testimonial, 'app__testimonial'),
-  'testimonial',
+  'testimonials',
   'app__primarybg',
 );
