84661cc6f8be85af9d1b8244a83a0089d37282fe
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-02-02 15:44:10 +0100

    Change the app deployment URL, minor changes

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -9,7 +9,7 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
         <Component />
 
         <div className="copyright">
-          <p className="p-text">@2022 AXE_BIT</p>
+          <p className="p-text">@2022-2023 AXE_BIT</p>
           <p className="p-text">All rights reserved</p>
         </div>
       </div>
