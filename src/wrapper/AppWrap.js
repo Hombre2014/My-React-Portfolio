086b913cefdbc2d0ea0eff30c6d97d131665f8f1
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2025-05-19 09:18:37 +0200

    Upgrade of backend react and sanity. Make the changes on the frontend to work withthe updated backend.

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -7,11 +7,6 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
       <SocialMedia />
       <div className="app__wrapper app__flex">
         <Component />
-
-        <div className="copyright">
-          <p className="p-text">@2022-{new Date().getFullYear()} AXE_BIT</p>
-          <p className="p-text">All rights reserved</p>
-        </div>
       </div>
       <NavigationDots active={idName} />
     </div>
